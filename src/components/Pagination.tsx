9a5c97cf596b80643836e07807a4230762fa0d68
Hak9ya <jhy2297@naver.com>
2022-06-15 17:56:22 +0900

    페이징 수정

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -10,27 +10,33 @@ const Pagination = ({
   const [blockNum, setBlockNum] = useState(0);
   const [currPage, setCurrPage] = useState(1);
 
-  const v = blockNum * 5;
+  const startPage = blockNum * 5;
+  const endPage = 5 + startPage;
+
   const pageNumbers = [];
   for (let i = 1; i <= Math.ceil(total / limit); i++) {
     pageNumbers.push(i);
   }
-  const pageNumberSlice = pageNumbers.slice(v, 5 + v);
+  const pageNumberSlice = pageNumbers.slice(startPage, endPage);
 
   const prevPage = () => {
-    if (currPage <= 1) return;
-    if (currPage - 1 <= 5 * blockNum) {
+    if (blockNum <= 0) {
+      alert('더 이상 못감');
+      return;
+    } else if (1 <= blockNum) {
       setBlockNum((n) => n - 1);
+      setCurrPage((n) => n - 5);
     }
-    setCurrPage((n) => n - 1);
   };
 
   const nextPage = () => {
-    if (currPage >= Math.ceil(total / limit)) return;
-    if (5 * (blockNum + 1) < currPage + 1) {
+    if (blockNum + 1 >= Math.ceil(Math.ceil(total / limit)) / 5) {
+      alert('더 이상 못감');
+      return;
+    } else if (blockNum < Math.ceil(Math.ceil(total / limit)) / 5) {
       setBlockNum((n) => n + 1);
+      setCurrPage((n) => n + 5);
     }
-    setCurrPage((n) => n + 1);
   };
 
   return (
